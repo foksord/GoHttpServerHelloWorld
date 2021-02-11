f6fa67b6916340aceb0ca7e544ca0f591129681a
Fatih YOLCU <foksord@gmail.com>
2021-02-11 20:59:51 +0300

    init

---
+++ helloworld.go
@@ -0,0 +1,39 @@
+package main
+
+// [START import]
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/foksord/portChecker"
+)
+
+func main() {
+
+	port, err := portChecker.PortChecker()
+	if err != nil {
+		panic(err.Error())
+	}
+	var sb strings.Builder
+	sb.WriteString(":")
+	sb.WriteString(strconv.FormatInt(int64(port), 10))
+	log.Printf("Listening on port %s", sb.String())
+
+	http.HandleFunc("/", indexHandler)
+
+	log.Printf("Listening on port %s", sb.String())
+	if err := http.ListenAndServe(sb.String(), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexHandler(writeRes http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(writeRes, req)
+		return
+	}
+	fmt.Fprint(writeRes, "Hello, World!")
+}
